618fee065dd5871e085a14c0ecb245d859ee8895
Ryan <ryanvitorconta2@gmail.com>
2024-05-12 23:14:32 -0300

    atualizando codigo javascript

--- js/login.js
+++ js/login.js
@@ -3,15 +3,5 @@ function clearInput(inputId) {
     document.getElementById(inputId).value = "";
 }
 
-function togglePasswordVisibility() {
-    var senhaInput = document.getElementById('senha');
-    
-    if (senhaInput.type === 'password') {
-        senhaInput.type = 'text'; // Se a senha estiver oculta, exibe-a
-        senhaInput.style.width = senhaInput.offsetWidth + 'px'; // Mantém a largura do campo de senha
-    } else {
-        senhaInput.type = 'password'; // Se a senha estiver visível, oculta-a
-    }
-}
 
 
--- js/script.js
+++ js/script.js
@@ -16,58 +16,5 @@
   $(window).on('load', function() {
     $("#loader-wrapper").fadeOut(700);
   });
+});
 
-  // Função para verificar se uma string contém apenas letras
-  function containsOnlyLetters(str) {
-    return /^[a-zA-Z]+$/.test(str);
-  }
-
-  // Função para validar o email
-  function isValidEmail(email) {
-    // Expressão regular para validar o email
-    return /^[^\s@]+@[^\s@]+\.[^\s@]+$/.test(email);
-  }
-
-  // Evento de desfoco para o campo de nome
-  $('#nome').on('blur', function() {
-    var nome = $(this).val();
-    // Verifica se o campo está vazio
-    if (nome.trim() === '') {
-      alert('Por favor, preencha o campo Nome.');
-      return;
-    } else if (!containsOnlyLetters(nome.trim())) { // Verifica se contém apenas letras
-      alert('Por favor, preencha APENAS COM LETRAS o campo Nome.');
-      return;
-    }
-  });
-
-  // Evento de desfoco para o campo de email
-  $('#email').on('blur', function() {
-    var email = $(this).val();
-    // Verifica se o campo está vazio
-    if (email.trim() === '') {
-      alert('Por favor, preencha o campo Email.');
-      return;
-    } else if (!isValidEmail(email.trim())) { // Verifica se é um email válido
-      alert('Por favor, preencha um email válido.');
-      return;
-    }
-  });
-
-  // Evento de desfoco para o campo de senha
-  $('#senha').on('blur', function() {
-    var senha = $(this).val();
-    // Verifica se o campo está vazio
-    if (senha.trim() === '') {
-      alert('Por favor, preencha o campo Senha.');
-      return;
-    }
-
-    // Verifica se a senha tem pelo menos 6 caracteres
-    if (senha.length < 6) {
-      alert('A senha deve ter pelo menos 6 caracteres.');
-      return;
-    }
-  });
-
-})(jQuery);
